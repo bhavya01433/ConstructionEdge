7a05a8ec9135a35ed779516993fdd04a0088d899
bhavya01433 <bhavyatrivedi501@gmail.com>
2025-06-09 18:57:05 +0530

    Footer added

--- src/pages/portfolio/MasterpieceDetail.jsx
+++ src/pages/portfolio/MasterpieceDetail.jsx
@@ -1,7 +1,11 @@
 import { Link } from "react-router-dom";
+import { useEffect } from "react";
 import "./portfolioDetailed.css";
 
 const MasterpieceDetail = () => {
+  useEffect(() => {
+    window.scrollTo({ top: 0, behavior: "smooth" });
+  }, []);
   return (
     <div className="portfolio-detailed-container">
       {/* Hero Section */}
